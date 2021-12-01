db75f67e596bf02cef6e6f67f078e642e9b5e765
Julien Pinsonneau <jpinsonn@redhat.com>
2021-12-01 12:44:10 +0100

    removed mock test

--- web/src/components/__tests__/netflow-traffic.spec.tsx
+++ web/src/components/__tests__/netflow-traffic.spec.tsx
@@ -3,10 +3,6 @@ import { shallow } from 'enzyme';
 
 import NetflowTraffic from "../netflow-traffic";
 
-/*jest.mock('@console/internal/components/utils/k8s-watch-hook', () => ({
-  useK8sWatchResource: jest.fn(),
-}));*/
-
 describe("<NetflowTraffic />", () => {
   it('should render component', () => {
     const wrapper = shallow(
