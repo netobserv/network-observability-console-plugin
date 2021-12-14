0f1112a668e98457390bf5ac9303d4dfb69bf5e8
Julien Pinsonneau <jpinsonn@redhat.com>
2021-12-14 15:43:42 +0100

    added tests

--- web/src/components/__tests__/netflow-traffic.spec.tsx
+++ web/src/components/__tests__/netflow-traffic.spec.tsx
@@ -1,17 +1,25 @@
 import * as React from 'react';
-import { shallow, render } from 'enzyme';
+import { shallow, render, mount } from 'enzyme';
 import { useResolvedExtensions } from '@openshift-console/dynamic-plugin-sdk';
+import { getFlows } from '../../api/routes';
 import NetflowTraffic from "../netflow-traffic";
 import { extensionsMock } from "../__tests-data__/extensions";
+import { FlowsSample } from '../__tests-data__/flows';
 
 jest.mock('@openshift-console/dynamic-plugin-sdk', () => ({
   useResolvedExtensions: jest.fn(),
 }));
 const useResolvedExtensionsMock = useResolvedExtensions as jest.Mock;
 
+jest.mock('../../api/routes', () => ({
+  getFlows: jest.fn(),
+}));
+const getFlowsMock = getFlows as jest.Mock;
+
 describe("<NetflowTraffic />", () => {
   beforeAll(() => {
     useResolvedExtensionsMock.mockReturnValue(extensionsMock);
+    getFlowsMock.mockResolvedValue(FlowsSample)
   });
 
   it('should shallow component', () => {
@@ -26,5 +34,16 @@ describe("<NetflowTraffic />", () => {
     );
     expect(cheerio.find('#refresh-dropdown').length).toEqual(1);
     expect(cheerio.find('#refresh-button').length).toEqual(1);
+  });
+  it('should refresh on button click', () => {
+    const wrapper = mount(
+      <NetflowTraffic />
+    );
+    //should show flows at first load
+    expect(getFlowsMock).toHaveBeenCalledTimes(1);
+
+    //should have called getFlow twice after click
+    wrapper.find('#refresh-button').at(0).simulate('click');
+    expect(getFlowsMock).toHaveBeenCalledTimes(2);
   });
 });
--- web/src/components/__tests__/refresh-dropdown.spec.tsx
+++ web/src/components/__tests__/refresh-dropdown.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { mount, shallow } from 'enzyme';
 
 import RefreshDropdown from "../refresh-dropdown";
 
@@ -7,11 +7,51 @@ describe("<RefreshDropdown />", () => {
   const props = {
     interval: null,
     setInterval: jest.fn(),
+    id: "refresh",
   }
   it('should render component', () => {
     const wrapper = shallow(
       <RefreshDropdown {...props} />
     );
     expect(wrapper.find(RefreshDropdown)).toBeTruthy();
+  });
+  it('should open and close', () => {
+    const wrapper = mount(
+      <RefreshDropdown {...props} />
+    );
+
+    const dropdown = wrapper.find('#refresh-dropdown');
+    expect(wrapper.find('li').length).toBe(0);
+    //open dropdow
+    dropdown.at(0).simulate('click');
+    expect(wrapper.find('li').length).toBeGreaterThan(0);
+
+    //close dropdow
+    dropdown.at(0).simulate('click');
+    expect(wrapper.find('li').length).toBe(0);
+
+    //no setInterval should be called
+    expect(props.setInterval).toHaveBeenCalledTimes(0);
+  });
+  it('should refresh on select', () => {
+    const wrapper = mount(
+      <RefreshDropdown {...props} />
+    );
+
+    const dropdown = wrapper.find('#refresh-dropdown');
+    //open dropdown and select refresh off
+    dropdown.at(0).simulate('click');
+    wrapper.find('[id="OFF_KEY"]').at(0).simulate('click');
+    expect(props.setInterval).toHaveBeenCalledWith(null);
+    expect(wrapper.find('li').length).toBe(0);
+
+    //open dropdown and select 15s
+    dropdown.at(0).simulate('click');
+    wrapper.find('[id="15s"]').at(0).simulate('click');
+    expect(props.setInterval).toHaveBeenCalledWith(15000);
+    expect(wrapper.find('li').length).toBe(0);
+
+    //setInterval should be called twice 
+    expect(props.setInterval).toHaveBeenCalledTimes(2);
   });
 });
