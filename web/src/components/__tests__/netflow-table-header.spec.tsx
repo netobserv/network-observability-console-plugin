247ccb5b7bf6ced6432b19a0f794369d058b72f2
Olivier Cazade <ocazade@redhat.com>
2021-12-14 10:52:27 +0100

    Added sort test on netflow-table-header

--- web/src/components/__tests__/netflow-table-header.spec.tsx
+++ web/src/components/__tests__/netflow-table-header.spec.tsx
@@ -1,32 +1,50 @@
 import * as React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
+import { TableComposable, Tbody, OnSort } from "@patternfly/react-table";
 
-import { NetflowTableHeader } from "../netflow-table-header";
+import { NetflowTableHeader, Column } from "../netflow-table-header";
 
 import { ColumnsSample } from "../__tests-data__/columns";
 import { Thead, Tr, Th } from "@patternfly/react-table";
 
+const NetflowTableHeaderWrapper: React.FC<{
+  onSort: OnSort;
+  sortIndex: number;
+  sortDirection: string;
+  columns: Column[];
+}> = ({ onSort, sortIndex, sortDirection, columns }) => {
+  return (
+    <TableComposable aria-label="Misc table" variant="compact">
+      <NetflowTableHeader
+        onSort={onSort}
+        sortDirection={sortDirection}
+        sortIndex={sortIndex}
+        columns={columns}
+      />
+      <Tbody></Tbody>
+    </TableComposable>
+  );
+};
+
 describe("<NetflowTableHeader />", () => {
   it("should render component", () => {
-      // eslint-disable-next-line
-      const onSort = () => {};
-      const wrapper = shallow(
-	  <NetflowTableHeader
-              onSort={onSort}
-              sortIndex={0}
-              sortDirection={"asc"}
-              columns={ColumnsSample}
-	  />
-      );
+    const onSort = jest.fn();
+    const wrapper = shallow(
+      <NetflowTableHeader
+        onSort={onSort}
+        sortIndex={0}
+        sortDirection={"asc"}
+        columns={ColumnsSample}
+      />
+    );
     expect(wrapper.find(NetflowTableHeader)).toBeTruthy();
     expect(wrapper.find(Thead)).toHaveLength(1);
     expect(wrapper.find(Tr)).toHaveLength(1);
     expect(wrapper.find(Th)).toHaveLength(ColumnsSample.length);
   });
   it("should render given columns", () => {
-      // eslint-disable-next-line
-      const onSort = () => {};
-      const reducedColumns = ColumnsSample.slice(2, 4);
+    const onSort = jest.fn();
+    const reducedColumns = ColumnsSample.slice(2, 4);
     const wrapper = shallow(
       <NetflowTableHeader
         onSort={onSort}
@@ -39,5 +57,19 @@ describe("<NetflowTableHeader />", () => {
     expect(wrapper.find(Thead)).toHaveLength(1);
     expect(wrapper.find(Tr)).toHaveLength(1);
     expect(wrapper.find(Th)).toHaveLength(reducedColumns.length);
+  });
+  it("should call sort function on click", () => {
+    const onSort = jest.fn();
+    const wrapper = mount(
+      <NetflowTableHeaderWrapper
+        onSort={onSort}
+        sortIndex={0}
+        sortDirection={"asc"}
+        columns={ColumnsSample}
+      />
+    );
+    expect(wrapper.find(NetflowTableHeader)).toBeTruthy();
+    wrapper.find("button").at(0).simulate("click");
+    expect(onSort).toHaveBeenCalled();
   });
 });
