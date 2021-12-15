b2defa4d19350b52b02976f66cfe322d9f01208b
Olivier Cazade <ocazade@redhat.com>
2021-12-15 16:51:15 +0100

    NETOBSERV-105 Netflow table sort test (#19)
    * Added sort test on netflow-table-header
    
    * Fix key bug in netflowtablerow
    
    * Added sort test for netflowtable
    
    * Added argument check in table row sorting test

--- web/src/components/__tests__/netflow-table.spec.tsx
+++ web/src/components/__tests__/netflow-table.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 import NetflowTable from '../netflow-table';
 import NetflowTableRow from '../netflow-table-row';
@@ -9,10 +9,19 @@ import { ParsedStream } from '../../api/loki';
 import { ColumnsSample } from '../__tests-data__/columns'
 import { FlowsSample } from '../__tests-data__/flows'
 
+jest.mock("@openshift-console/dynamic-plugin-sdk", () => {
+  return {
+    ResourceLink: () => {
+      return <></>;
+    },
+  };
+});
+
 describe("<NetflowTable />", () => {
     let flows:ParsedStream[] = []
-    const setFlows = (f) => {flows = f; return}
+    const setFlows = jest.fn();
     it('should render component', () => {
+	flows = FlowsSample.slice(0,FlowsSample.length)
  	const wrapper = shallow(
  	    <NetflowTable flows={flows} setFlows={setFlows} columns={ColumnsSample} />
  	);
@@ -20,7 +29,7 @@ describe("<NetflowTable />", () => {
 	expect(wrapper.find(NetflowTableHeader)).toHaveLength(1);
     });
     it('should have table rows', () => {
-	flows = FlowsSample
+	flows = FlowsSample.slice(0,FlowsSample.length)
  	const wrapper = shallow(
  	    <NetflowTable flows={flows} setFlows={setFlows} columns={ColumnsSample} />
  	);
@@ -28,10 +37,24 @@ describe("<NetflowTable />", () => {
 	expect(wrapper.find(NetflowTableRow)).toHaveLength(FlowsSample.length);
     });
     it('should only render given row', () => {
+	flows = FlowsSample.slice(0, 2)
  	const wrapper = shallow(
  	    <NetflowTable flows={flows} setFlows={setFlows} columns={ColumnsSample} />
  	);
  	expect(wrapper.find(NetflowTable)).toBeTruthy();
-	expect(wrapper.find(NetflowTableRow)).toHaveLength(FlowsSample.length);
+	expect(wrapper.find(NetflowTableRow)).toHaveLength(flows.length);
+    });
+    it('should sort rows on click', () => {
+	flows = FlowsSample.slice(0,FlowsSample.length)
+ 	const wrapper = mount(
+ 	    <NetflowTable flows={flows} setFlows={setFlows} columns={ColumnsSample} />
+ 	);
+ 	expect(wrapper.find(NetflowTable)).toBeTruthy();
+	const button = wrapper.findWhere(node => {
+	    return node.type() === 'button' && node.text() === "Date & time";
+	});
+	button.simulate("click");
+	const expected = [FlowsSample[2], FlowsSample[0], FlowsSample[1]];
+	expect(setFlows).toHaveBeenCalledWith(expected);
     });
 });
