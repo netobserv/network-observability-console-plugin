e9fa5ecbb7a2c356f4931cc09be0c2caf50f479c
Olivier Cazade <ocazade@redhat.com>
2021-12-22 12:00:21 +0100

    Changed port sort behavior to have numerical port last

--- web/src/utils/port.ts
+++ web/src/utils/port.ts
@@ -9,3 +9,20 @@ export const formatPort = p => {
     return String(p);
   }
 };
+
+// This sort is ordering first the port with a known service
+// then order numerically the other port
+export const comparePort = (p1, p2) => {
+  const s1 = getService(p1);
+  const s2 = getService(p2);
+  if (s1 && s2) {
+    return formatPort(p1).localeCompare(formatPort(p2));
+  }
+  if (s1) {
+    return -1;
+  }
+  if (s2) {
+    return 1;
+  }
+  return p1 - p2;
+};
