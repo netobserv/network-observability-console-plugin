dcbad365bd97ff4173e5bc59fa235bd344c6960d
Julien Pinsonneau <jpinsonn@redhat.com>
2022-02-10 10:59:38 +0100

    hot reloading + devdependencies

--- web/webpack.config.ts
+++ web/webpack.config.ts
@@ -1,13 +1,21 @@
+
 /* eslint-env node */
 
-import * as webpack from 'webpack';
+import { Configuration as WebpackConfiguration } from "webpack";
+import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
 import * as path from 'path';
 import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
 
+interface Configuration extends WebpackConfiguration {
+  devServer?: WebpackDevServerConfiguration;
+}
+
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-const config: webpack.Configuration = {
+const config: Configuration = {
   mode: 'development',
+  // No regular entry points. The remote container entry is handled by ConsoleRemotePlugin.
+  entry: {},
   context: path.resolve(__dirname, 'src'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -62,6 +70,25 @@ const config: webpack.Configuration = {
         ],
       },
     ],
+  },
+  devServer: {
+    static: './dist',
+    port: 9001,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
+    },
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    proxy: {
+      '/api': {
+           target: 'http://localhost:9000',
+           router: () => 'http://localhost:9002',
+           logLevel: 'debug' /*optional*/
+      }
+   }
   },
   plugins: [
     new ConsoleRemotePlugin(),
