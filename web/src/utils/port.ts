c6f2fb843942260ae53d081b0eacc8d506d4ff92
Olivier Cazade <ocazade@redhat.com>
2022-02-24 11:19:28 +0100

    Restrict port to service formatting to ports below 1024 (#80)

--- web/src/utils/port.ts
+++ web/src/utils/port.ts
@@ -1,7 +1,15 @@
 import { getService } from 'port-numbers';
 
+export const getProtectedService = (p: number) => {
+  if (p < 1024) {
+    return getService(p);
+  } else {
+    return null;
+  }
+};
+
 export const formatPort = (p: number) => {
-  const service = getService(p);
+  const service = getProtectedService(p);
   if (service) {
     return `${service.name} (${p})`;
   } else {
@@ -12,8 +20,8 @@ export const formatPort = (p: number) => {
 // This sort is ordering first the port with a known service
 // then order numerically the other port
 export const comparePorts = (p1: number, p2: number) => {
-  const s1 = getService(p1);
-  const s2 = getService(p2);
+  const s1 = getProtectedService(p1);
+  const s2 = getProtectedService(p2);
   if (s1 && s2) {
     return formatPort(p1).localeCompare(formatPort(p2));
   }
