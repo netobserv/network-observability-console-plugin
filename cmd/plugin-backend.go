5c7cf79beb18e0c5427bde3af32344b96bf239ca
Joel Takvorian <jtakvori@redhat.com>
2022-03-01 17:16:48 +0100

    Move to github actions for quay images pushed
    The action provides an image tagged "main" and another tagged with the
    commit SHA, set with 2 weeks expiry

--- cmd/plugin-backend.go
+++ cmd/plugin-backend.go
@@ -15,15 +15,16 @@ import (
 )
 
 var (
-	version     = "unknown"
-	app         = "console-plugin-backend"
-	port        = flag.Int("port", 9001, "server port to listen on (default: 9001)")
-	cert        = flag.String("cert", "", "cert file path to enable TLS (disabled by default)")
-	key         = flag.String("key", "", "private key file path to enable TLS (disabled by default)")
-	corsOrigin  = flag.String("cors-origin", "*", "CORS allowed origin (default: *)")
-	corsMethods = flag.String("cors-methods", "", "CORS allowed methods (default: unset)")
-	corsHeaders = flag.String("cors-headers", "Origin, X-Requested-With, Content-Type, Accept", "CORS allowed headers (default: Origin, X-Requested-With, Content-Type, Accept)")
-	corsMaxAge  = flag.String("cors-max-age", "", "CORS allowed max age (default: unset)")
+	buildVersion = "unknown"
+	buildDate    = "unknown"
+	app          = "netobserv-console-plugin"
+	port         = flag.Int("port", 9001, "server port to listen on (default: 9001)")
+	cert         = flag.String("cert", "", "cert file path to enable TLS (disabled by default)")
+	key          = flag.String("key", "", "private key file path to enable TLS (disabled by default)")
+	corsOrigin   = flag.String("cors-origin", "*", "CORS allowed origin (default: *)")
+	corsMethods  = flag.String("cors-methods", "", "CORS allowed methods (default: unset)")
+	corsHeaders  = flag.String("cors-headers", "Origin, X-Requested-With, Content-Type, Accept", "CORS allowed headers (default: Origin, X-Requested-With, Content-Type, Accept)")
+	corsMaxAge   = flag.String("cors-max-age", "", "CORS allowed max age (default: unset)")
 	// todo: default value temporarily kept to make it work with older versions of the NOO. Remove default and force setup of loki url
 	lokiURL      = flag.String("loki", "http://localhost:3100", "URL of the loki querier host")
 	lokiLabels   = flag.String("loki-labels", "SrcNamespace,SrcWorkload,DstNamespace,DstWorkload,FlowDirection", "Loki labels, comma separated")
@@ -31,13 +32,13 @@ var (
 	lokiTenantID = flag.String("loki-tenant-id", "", "Tenant organization ID for multi-tenant-loki (submitted as the X-Scope-OrgID HTTP header)")
 	logLevel     = flag.String("loglevel", "info", "log level (default: info)")
 	versionFlag  = flag.Bool("v", false, "print version")
-	appVersion   = fmt.Sprintf("%s %s", app, version)
 	log          = logrus.WithField("module", "main")
 )
 
 func main() {
 	flag.Parse()
 
+	appVersion := fmt.Sprintf("%s [build version: %s, build date: %s]", app, buildVersion, buildDate)
 	if *versionFlag {
 		fmt.Println(appVersion)
 		os.Exit(0)
