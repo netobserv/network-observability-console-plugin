98c33b88ce23b674012a6b470f999ec8c17f7b35
Joel Takvorian <jtakvori@redhat.com>
2022-03-02 16:12:41 +0100

    Remove loki dependency (#88)
    Just declare the few model / struct we actually depend on

--- go.mod
+++ go.mod
@@ -1,11 +1,21 @@
 module github.com/netobserv/network-observability-console-plugin
 
-go 1.16
+go 1.17
 
 require (
 	github.com/gorilla/mux v1.8.0
-	github.com/grafana/loki v1.6.1
+	github.com/json-iterator/go v1.1.12
+	github.com/prometheus/common v0.32.1
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/objx v0.3.0 // indirect
 	github.com/stretchr/testify v1.5.1
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.3.0 // indirect
+	golang.org/x/sys v0.0.0-20210603081109-ebe580a85c40 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pkg/model/loki.go
+++ pkg/model/loki.go
@@ -3,57 +3,160 @@ package model
 import (
 	"fmt"
 	"strconv"
+	"time"
+
+	json "github.com/json-iterator/go"
+	"github.com/prometheus/common/model"
 )
 
-type LokiQuery struct {
-	Query string
-	Limit int
-	Start int64
-	End   int64
+// QueryResponse represents the http json response to a label query
+type QueryResponse struct {
+	Status string            `json:"status"`
+	Data   QueryResponseData `json:"data"`
 }
 
-// TODO: unclear yet if all this model is useful. Currently unused, as this is done on the frontend side
-type LokiStreamResponse struct {
-	ResultType string       `json:"resultType"`
-	Result     StreamResult `json:"result"`
-	Stats      LokiStats    `json:"stats"`
+// ResultType holds the type of the result
+type ResultType string
+
+// ResultType values
+const (
+	ResultTypeStream = "streams"
+	ResultTypeScalar = "scalar"
+	ResultTypeVector = "vector"
+	ResultTypeMatrix = "matrix"
+)
+
+// ResultValue interface mimics the promql.Value interface
+type ResultValue interface {
+	Type() ResultType
 }
 
-type LokiStats struct {
+//QueryResponseData represents the http json response to a label query
+type QueryResponseData struct {
+	ResultType ResultType  `json:"resultType"`
+	Result     ResultValue `json:"result"`
 }
 
-type StreamResult struct {
-	Stream map[string]string `json:"stream"`
-	Values [][]string        `json:"values"`
+// Type implements the promql.Value interface
+func (Streams) Type() ResultType { return ResultTypeStream }
+
+// Type implements the promql.Value interface
+func (Scalar) Type() ResultType { return ResultTypeScalar }
+
+// Type implements the promql.Value interface
+func (Vector) Type() ResultType { return ResultTypeVector }
+
+// Type implements the promql.Value interface
+func (Matrix) Type() ResultType { return ResultTypeMatrix }
+
+// Streams is a slice of Stream
+type Streams []Stream
+
+//Stream represents a log stream.  It includes a set of log entries and their labels.
+type Stream struct {
+	Labels  map[string]string `json:"stream"`
+	Entries []Entry           `json:"values"`
 }
 
-type ParsedStream struct {
-	Labels map[string]string   `json:"labels"`
-	Values []ParsedStreamValue `json:"values"`
+//Entry represents a log entry.  It includes a log message and the time it occurred at.
+type Entry struct {
+	Timestamp time.Time
+	Line      string
 }
 
-type ParsedStreamValue struct {
-	Timestamp int64  `json:"timestamp"`
-	Blob      string `json:"blob"` // TODO: expand to IPFIX fields
-}
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (q *QueryResponseData) UnmarshalJSON(data []byte) error {
+	unmarshal := struct {
+		Type   ResultType      `json:"resultType"`
+		Result json.RawMessage `json:"result"`
+	}{}
 
-func (in *StreamResult) Parse() (*ParsedStream, error) {
-	parsedValues := []ParsedStreamValue{}
-	for _, v := range in.Values {
-		if len(v) < 2 {
-			return nil, fmt.Errorf("parsing Loki result failed, unexpected stream values length: %v", v)
-		}
-		timestamp, err := strconv.ParseInt(v[0], 10, 64)
-		if err != nil {
-			return nil, fmt.Errorf("parsing Loki result failed, invalid timestamp [%s]: %w", v[0], err)
-		}
-		parsedValues = append(parsedValues, ParsedStreamValue{
-			Timestamp: timestamp,
-			Blob:      v[1],
-		})
+	err := json.Unmarshal(data, &unmarshal)
+	if err != nil {
+		return err
 	}
-	return &ParsedStream{
-		Labels: in.Stream,
-		Values: parsedValues,
-	}, nil
+
+	var value ResultValue
+
+	// unmarshal results
+	switch unmarshal.Type {
+	case ResultTypeStream:
+		var s Streams
+		err = json.Unmarshal(unmarshal.Result, &s)
+		value = s
+	case ResultTypeMatrix:
+		var m Matrix
+		err = json.Unmarshal(unmarshal.Result, &m)
+		value = m
+	case ResultTypeVector:
+		var v Vector
+		err = json.Unmarshal(unmarshal.Result, &v)
+		value = v
+	case ResultTypeScalar:
+		var v Scalar
+		err = json.Unmarshal(unmarshal.Result, &v)
+		value = v
+	default:
+		return fmt.Errorf("unknown type: %s", unmarshal.Type)
+	}
+
+	if err != nil {
+		return err
+	}
+
+	q.ResultType = unmarshal.Type
+	q.Result = value
+
+	return nil
 }
+
+// MarshalJSON implements the json.Marshaler interface.
+func (e *Entry) MarshalJSON() ([]byte, error) {
+	l, err := json.Marshal(e.Line)
+	if err != nil {
+		return nil, err
+	}
+	return []byte(fmt.Sprintf("[\"%d\",%s]", e.Timestamp.UnixNano(), l)), nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+func (e *Entry) UnmarshalJSON(data []byte) error {
+	var unmarshal []string
+
+	err := json.Unmarshal(data, &unmarshal)
+	if err != nil {
+		return err
+	}
+
+	t, err := strconv.ParseInt(unmarshal[0], 10, 64)
+	if err != nil {
+		return err
+	}
+
+	e.Timestamp = time.Unix(0, t)
+	e.Line = unmarshal[1]
+
+	return nil
+}
+
+// Scalar is a single timestamp/float with no labels
+type Scalar model.Scalar
+
+func (s Scalar) MarshalJSON() ([]byte, error) {
+	return model.Scalar(s).MarshalJSON()
+}
+
+func (s *Scalar) UnmarshalJSON(b []byte) error {
+	var v model.Scalar
+	if err := v.UnmarshalJSON(b); err != nil {
+		return err
+	}
+	*s = Scalar(v)
+	return nil
+}
+
+// Vector is a slice of Samples
+type Vector []model.Sample
+
+// Matrix is a slice of SampleStreams
+type Matrix []model.SampleStream
