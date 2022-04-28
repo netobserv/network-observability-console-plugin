ec5752e4cfa200b2f654741c4685499c21b6e56b
Joel Takvorian <jtakvori@redhat.com>
2022-04-28 18:06:35 +0200

    Fix failing test (several datetime classes)

--- web/src/components/netflow-table/__tests__/netflow-table.spec.tsx
+++ web/src/components/netflow-table/__tests__/netflow-table.spec.tsx
@@ -9,6 +9,7 @@ import { NetflowTableHeader } from '../netflow-table-header';
 import { ShuffledDefaultColumns } from '../../__tests-data__/columns';
 import { FlowsSample } from '../../__tests-data__/flows';
 import { Size } from '../../dropdowns/display-dropdown';
+import { ColumnsId } from '../../../utils/columns';
 
 const errorStateQuery = `EmptyState[data-test="error-state"]`;
 const loadingContentsQuery = `Bullseye[data-test="loading-contents"]`;
@@ -68,10 +69,10 @@ describe('<NetflowTable />', () => {
     const button = wrapper.findWhere(node => {
       return node.type() === 'button' && node.text() === 'Date & time';
     });
+    const timestampIdx = ShuffledDefaultColumns.findIndex(c => c.id === ColumnsId.timestamp);
     button.simulate('click');
-    const expectedDateText =
-      new Date(FlowsSample[2].timestamp).toDateString() + ' ' + new Date(FlowsSample[2].timestamp).toLocaleTimeString();
-    expect(wrapper.find(NetflowTableRow).find(Td).find('.datetime').at(0).text()).toBe(expectedDateText);
+    const expectedDateText = new Date(FlowsSample[2].timestamp).toDateString() + ' ' + new Date(FlowsSample[2].timestamp).toLocaleTimeString();
+    expect(wrapper.find(NetflowTableRow).find(Td).at(timestampIdx).find('.datetime').at(0).text()).toBe(expectedDateText);
     const expectedSrcAddress = FlowsSample[2].fields.SrcAddr;
     expect(wrapper.find(NetflowTableRow).at(0).text()).toContain(expectedSrcAddress);
     const expectedDstAddress = FlowsSample[2].fields.DstAddr;
