997ac0e8078d93af76ce7d177b81facd33e1c089
Julien Pinsonneau <jpinsonn@redhat.com>
2022-10-25 16:07:12 +0200

    fixed tests

--- web/src/components/__tests__/netflow-traffic.spec.tsx
+++ web/src/components/__tests__/netflow-traffic.spec.tsx
@@ -3,18 +3,23 @@ import { mount, render, shallow } from 'enzyme';
 import * as React from 'react';
 import { waitFor } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
-import { getFlows } from '../../api/routes';
+import { getFlows, getTopology } from '../../api/routes';
 import NetflowTraffic from '../netflow-traffic';
 import { extensionsMock } from '../__tests-data__/extensions';
 import { FlowsResultSample } from '../__tests-data__/flows';
 import NetflowTrafficParent from '../netflow-traffic-parent';
+import { TopologyResult } from '../../api/loki';
 
 const useResolvedExtensionsMock = useResolvedExtensions as jest.Mock;
 
 jest.mock('../../api/routes', () => ({
-  getFlows: jest.fn(() => Promise.resolve(FlowsResultSample))
+  getFlows: jest.fn(() => Promise.resolve(FlowsResultSample)),
+  getTopology: jest.fn(() =>
+    Promise.resolve({ metrics: [], stats: { numQueries: 0, limitReached: false } } as TopologyResult)
+  )
 }));
 const getFlowsMock = getFlows as jest.Mock;
+const getTopologyMock = getTopology as jest.Mock;
 
 describe('<NetflowTraffic />', () => {
   beforeAll(() => {
@@ -36,13 +41,14 @@ describe('<NetflowTraffic />', () => {
   });
 
   it('should refresh on button click', async () => {
-    const wrapper = mount(<NetflowTrafficParent />);
     act(() => {
-      //should have called getFlow twice after click
+      const wrapper = mount(<NetflowTrafficParent />);
+      //should have called getTopology 4 times after click (2 for current scope and 2 for app)
       wrapper.find('#refresh-button').at(0).simulate('click');
     });
     await waitFor(() => {
-      expect(getFlowsMock).toHaveBeenCalledTimes(1);
+      expect(getFlowsMock).toHaveBeenCalledTimes(0);
+      expect(getTopologyMock).toHaveBeenCalledTimes(4);
     });
   });
 
@@ -50,7 +56,7 @@ describe('<NetflowTraffic />', () => {
     act(() => {
       const cheerio = render(<NetflowTrafficParent />);
       expect(cheerio.find('#filter-toolbar').length).toEqual(1);
-      expect(cheerio.find('#manage-columns-button').length).toEqual(1);
+      expect(cheerio.find('#manage-overview-panels-button').length).toEqual(1);
     });
   });
 });
