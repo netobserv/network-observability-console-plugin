3e15f2e9ab15ee95f89b8230dc8184baa5b11fcf
Julien Pinsonneau <jpinsonn@redhat.com>
2022-11-23 09:58:43 +0100

    fix update columns on reorder

--- web/src/components/netflow-traffic.tsx
+++ web/src/components/netflow-traffic.tsx
@@ -820,7 +820,7 @@ export const NetflowTraffic: React.FC<{
             size={size}
             onSelect={onRecordSelect}
             columns={columns.filter(col => col.isSelected)}
-            setColumns={setColumns}
+            setColumns={(v: Column[]) => setColumns(v.concat(columns.filter(col => !col.isSelected)))}
             filterActionLinks={filterLinks()}
             isDark={isDarkTheme}
           />
