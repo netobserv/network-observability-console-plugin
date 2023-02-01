3badecfc675f63d7f497c4d86c296a0b9ac267e7
Joel Takvorian <jtakvori@redhat.com>
2023-02-01 12:32:10 +0100

    NETOBSERV-855 add authentication checks

---
+++ pkg/handler/auth/check_auth.go
@@ -0,0 +1,58 @@
+package auth
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"strings"
+
+	"github.com/sirupsen/logrus"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+)
+
+var hlog = logrus.WithField("module", "handler.auth")
+
+const AuthHeader = "Authorization"
+
+func GetUserToken(header http.Header) (string, error) {
+	authValue := header.Get(AuthHeader)
+	if authValue != "" {
+		parts := strings.Split(authValue, "Bearer ")
+		if len(parts) != 2 {
+			return "", errors.New("missing Bearer token in Authorization header")
+		}
+		return parts[1], nil
+	}
+	return "", errors.New("missing Authorization header")
+}
+
+func CheckAuth(ctx context.Context, header http.Header) error {
+	hlog.Debug("Checking authenticated user")
+	token, err := GetUserToken(header)
+	if err != nil {
+		return err
+	}
+	hlog.Debug("Checking auth: token found")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		return err
+	}
+	config.BearerToken = token
+	config.BearerTokenFile = ""
+
+	client, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return err
+	}
+	hlog.Debug("Checking auth: kube config created")
+
+	_, err = client.CoreV1().Namespaces().List(ctx, v1.ListOptions{})
+	if err != nil {
+		return err
+	}
+
+	hlog.Debug("Checking auth: passed")
+	return nil
+}
--- pkg/server/routes.go
+++ pkg/server/routes.go
@@ -1,16 +1,32 @@
 package server
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/sirupsen/logrus"
 
 	"github.com/netobserv/network-observability-console-plugin/pkg/handler"
+	"github.com/netobserv/network-observability-console-plugin/pkg/handler/auth"
 )
 
 func setupRoutes(cfg *Config) *mux.Router {
 	r := mux.NewRouter()
+	r.Use(func(orig http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if err := auth.CheckAuth(context.TODO(), r.Header); err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				_, err2 := w.Write([]byte(err.Error()))
+				if err2 != nil {
+					logrus.Errorf("Error while responding an error: %v (initial was: %v)", err2, err)
+				}
+				return
+			}
+			orig.ServeHTTP(w, r)
+		})
+	})
 	r.HandleFunc("/api/status", handler.Status)
 	r.HandleFunc("/api/loki/ready", handler.LokiReady(&cfg.Loki))
 	r.HandleFunc("/api/loki/metrics", handler.LokiMetrics(&cfg.Loki))
