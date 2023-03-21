e35befb3b0e1764bdd123333d5cebe090b88349e
Julien Pinsonneau <jpinsonn@redhat.com>
2023-03-21 13:39:48 +0100

    reload HOST token on query

--- pkg/loki/config.go
+++ pkg/loki/config.go
@@ -2,21 +2,17 @@ package loki
 
 import (
 	"net/url"
-	"os"
 	"time"
 
 	"github.com/netobserv/network-observability-console-plugin/pkg/utils"
-	"github.com/sirupsen/logrus"
 )
-
-var log = logrus.WithField("module", "loki config")
 
 type Config struct {
 	URL              *url.URL
 	StatusURL        *url.URL
 	Timeout          time.Duration
 	TenantID         string
-	Authorization    string
+	TokenPath        string
 	SkipTLS          bool
 	CAPath           string
 	UseMocks         bool
@@ -25,21 +21,12 @@ type Config struct {
 }
 
 func NewConfig(url *url.URL, statusURL *url.URL, timeout time.Duration, tenantID string, tokenPath string, forwardUserToken bool, skipTLS bool, capath string, useMocks bool, labels []string) Config {
-	authorization := ""
-	if tokenPath != "" {
-		bytes, err := os.ReadFile(tokenPath)
-		if err != nil {
-			log.WithError(err).Fatalf("failed to parse authorization path: %s", tokenPath)
-		}
-		authorization = "Bearer " + string(bytes)
-	}
-
 	return Config{
 		URL:              url,
 		StatusURL:        statusURL,
 		Timeout:          timeout,
 		TenantID:         tenantID,
-		Authorization:    authorization,
+		TokenPath:        tokenPath,
 		SkipTLS:          skipTLS,
 		CAPath:           capath,
 		UseMocks:         useMocks,
