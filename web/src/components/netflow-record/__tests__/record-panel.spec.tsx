53a93085d85d86399a52f3e71018be2a9522f21f
Julien Pinsonneau <jpinsonn@redhat.com>
2023-11-06 15:22:02 +0100

    fixed side panel testing + added case

--- web/src/components/netflow-record/__tests__/record-panel.spec.tsx
+++ web/src/components/netflow-record/__tests__/record-panel.spec.tsx
@@ -2,7 +2,7 @@ import { shallow } from 'enzyme';
 import * as React from 'react';
 import { FilterDefinitionSample, FiltersSample } from '../../__tests-data__/filters';
 import { DefaultColumnSample } from '../../__tests-data__/columns';
-import { FlowsSample } from '../../__tests-data__/flows';
+import { FlowsSample, UnknownFlow } from '../../__tests-data__/flows';
 import RecordPanel, { RecordDrawerProps } from '../record-panel';
 import { DrawerCloseButton } from '@patternfly/react-core';
 
@@ -26,8 +26,14 @@ describe('<RecordPanel />', () => {
     const wrapper = shallow(<RecordPanel {...mocks} />);
     expect(wrapper.find(RecordPanel)).toBeTruthy();
     expect(wrapper.find('#record-panel-test')).toHaveLength(1);
-    //all columns with data + JSON field
-    expect(wrapper.find('.record-field-container')).toHaveLength(40);
+    // all columns with data + JSON field
+    // sample contains 18 fields
+    // JSON tab represent 1 extra field
+    expect(wrapper.find('.record-field-container')).toHaveLength(18 + 1);
+
+    // same with 4 valid fields + json
+    wrapper.setProps({ record: UnknownFlow });
+    expect(wrapper.find('.record-field-container')).toHaveLength(4 + 1);
   });
   it('should close on click', async () => {
     const wrapper = shallow(<RecordPanel {...mocks} />);
