640d68acdbd3e5403db7072aa3b8af873068430c
Julien Pinsonneau <jpinsonn@redhat.com>
2024-03-13 09:38:52 +0100

    update config

---
+++ scripts/update-config.sh
@@ -0,0 +1,17 @@
+echo "Downloading config from controller repo..."
+mkdir -p ./tmp
+curl -o ./tmp/config.yaml https://raw.githubusercontent.com/netobserv/network-observability-operator/main/controllers/consoleplugin/config/static-frontend-config.yaml
+
+echo "Updating sample-config.yaml..."
+echo " - columns..."
+yq eval-all --inplace 'select(fileIndex==0).frontend.columns = select(fileIndex==1).columns | select(fileIndex==0)' ./config/sample-config.yaml ./tmp/config.yaml
+
+echo " - filters..."
+yq eval-all --inplace 'select(fileIndex==0).frontend.filters = select(fileIndex==1).filters | select(fileIndex==0)' ./config/sample-config.yaml ./tmp/config.yaml
+
+echo " - fields..."
+yq eval-all --inplace 'select(fileIndex==0).frontend.fields = select(fileIndex==1).fields | select(fileIndex==0)' ./config/sample-config.yaml ./tmp/config.yaml
+
+rm -rf ./tmp
+
+echo "Done !"
