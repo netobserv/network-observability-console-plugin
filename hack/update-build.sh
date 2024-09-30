7ed256c7f48c84d5bd0f3b4f01496e9d923818d8
Olivier Cazade <ocazade@redhat.com>
2024-09-30 14:40:36 +0000

    Addressing konflux error and fixing multi arch build with podman 5.2 (#595)

--- hack/update-build.sh
+++ hack/update-build.sh
@@ -15,4 +15,11 @@ LABEL maintainer="support@redhat.com"
 LABEL io.openshift.tags="network-observability-console-plugin"
 LABEL upstream-vcs-ref="${COMMIT}"
 LABEL upstream-vcs-type="git"
+LABEL description="Based on Openshift Console dynamic plugin, this plugin implement the console elements for Network Observability."
 EOF
+
+sed -i 's/\(FROM.*\)docker.io\/library\/golang:1.22\(.*\)/\1brew.registry.redhat.io\/rh-osbs\/openshift-golang-builder:v1.22.5-202407301806.g4c8b32d.el9\2/g' ./Dockerfile
+sed -i 's/\(FROM.*\)docker.io\/library\/node:18-alpine\(.*\)/\1registry.access.redhat.com\/ubi9\/nodejs-18:1-108.1716477799\2/g' ./Dockerfile
+
+sed -i 's/USER node//g' ./Dockerfile
+sed -i 's/--chown=node/--chown=default/g' ./Dockerfile
