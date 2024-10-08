d2a9636acc52939139fbe8af2419fd6e35c7b4da
Joel Takvorian <jtakvori@redhat.com>
2024-10-08 09:02:57 +0200

    NETOBSERV-1920: slider: switch to abbreviated mode with small screens (#613)
    Also adds a tooltip with more info
    
    apply left margin on tooltip
    
    Increase slider min size

--- web/src/components/slider/scope-slider.tsx
+++ web/src/components/slider/scope-slider.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useTranslation } from 'react-i18next';
 import { FlowScope } from '../../model/flow-query';
-import { Slider } from './slider';
+import { Slider, SliderStepObject } from './slider';
 
 import './scope-slider.css';
 
@@ -15,23 +15,68 @@ export interface ScopeSliderProps {
 export const ScopeSlider: React.FC<ScopeSliderProps> = ({ scope, setScope, allowedScopes, sizePx }) => {
   const { t } = useTranslation('plugin__netobserv-plugin');
 
-  let scopes: [FlowScope, string][] = [
-    ['resource', t('Resource')],
-    ['owner', t('Owner')],
-    ['namespace', t('Namespace')],
-    ['host', t('Node')],
-    ['zone', t('Zone')],
-    ['cluster', t('Cluster')]
-  ];
-  scopes = scopes.filter(s => allowedScopes.includes(s[0]));
-
-  const index = scopes.findIndex(s => s[0] === scope);
   /* TODO: refactor vertical slider
    * In between the display is block to working dimensions managing two cases
    * Non supported dimensions simply hide the slider from the view
    * since we can manage scopes from advanced view
    */
-  const canDisplay = sizePx > 450 && sizePx < 2000;
+  if (sizePx < 270 || sizePx > 2000) {
+    return null;
+  }
+
+  let scopes: [FlowScope, SliderStepObject][] = [
+    [
+      'resource',
+      {
+        value: 0,
+        label: sizePx > 450 ? t('Resource') : t('Res'),
+        tooltip: t('Base resource, such as a Pod, a Service or a Node')
+      }
+    ],
+    [
+      'owner',
+      {
+        value: 1,
+        label: sizePx > 450 ? t('Owner') : t('Own'),
+        tooltip: t('Controller owner, such as a Deployment')
+      }
+    ],
+    [
+      'namespace',
+      {
+        value: 2,
+        label: sizePx > 450 ? t('Namespace') : t('NS'),
+        tooltip: t('Resource namespace')
+      }
+    ],
+    [
+      'host',
+      {
+        value: 3,
+        label: sizePx > 450 ? t('Node') : t('Node'),
+        tooltip: t('Node on which the resources are running')
+      }
+    ],
+    [
+      'zone',
+      {
+        value: 4,
+        label: sizePx > 450 ? t('Zone') : t('AZ'),
+        tooltip: t('Availability zone')
+      }
+    ],
+    [
+      'cluster',
+      {
+        value: 5,
+        label: sizePx > 450 ? t('Cluster') : t('Cl'),
+        tooltip: t('Cluster name or identifier')
+      }
+    ]
+  ];
+  scopes = scopes.filter(s => allowedScopes.includes(s[0]));
+
+  const index = scopes.findIndex(s => s[0] === scope);
   const isBig = sizePx > 700;
   return (
     <div
@@ -42,16 +87,14 @@ export const ScopeSlider: React.FC<ScopeSliderProps> = ({ scope, setScope, allow
         left: -sizePx * (isBig ? 0.38 : 0.28)
       }}
     >
-      {canDisplay && (
-        <Slider
-          value={index < 0 ? 2 : index}
-          showTicks
-          max={scopes.length - 1}
-          customSteps={scopes.map((s, idx) => ({ value: idx, label: s[1] }))}
-          onChange={(value: number) => setScope(scopes[value][0])}
-          vertical
-        />
-      )}
+      <Slider
+        value={index < 0 ? 2 : index}
+        showTicks
+        max={scopes.length - 1}
+        customSteps={scopes.map(s => s[1])}
+        onChange={(value: number) => setScope(scopes[value][0])}
+        vertical
+      />
     </div>
   );
 };
