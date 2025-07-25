435f581ec4dee775876aee286a5b60a70761a7fc
Joel Takvorian <jtakvori@redhat.com>
2025-07-25 15:33:26 +0200

    Clean TS flow format code; remove field/label distinction
    The Label/Field hard distinction in the frontend isn't necessary to keep, and
    could become misleading as this is now configurable

--- web/src/utils/fields.ts
+++ web/src/utils/fields.ts
@@ -1,9 +1,11 @@
+import { Field } from '../api/ipfix';
+
 export type FieldType = 'string' | 'string[]' | 'number' | 'number[]';
 
 export type FieldFormat = 'IP';
 
 export interface FieldConfig {
-  name: string;
+  name: Field;
   type: FieldType;
   format?: FieldFormat;
   description: string;
